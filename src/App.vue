70fdac88ce65294e6cd9d24f7c3b10ae59aeba59
mspena14 <marlono1naranjo@gmail.com>
2024-08-05 08:14:51 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<Banner/>
+</template>
+
+<script setup lang="ts">
+import Banner from '@/components/Banner.vue'
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="banner-section w-100 p-10">
+        <form>
+            <div class="search-input d-middle">
+                <input type="text" required="true">
+                <span>Search movie</span>
+            </div>
+        </form>
+        <div class="container-banner-img">
+            <img :src="img" alt="">
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+    import svgImage from '@/assets/images/header-img.svg'
+    const img = svgImage
+</script>
+
+<style lang="scss" scoped>
+.banner-section{
+    width: 100%;
+    height: 50vh;
+    background-image: url(../assets/images/banner-bg.png);
+    background-size: cover;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    form{
+        .search-input{
+            
+        }
+    }
+
+    .container-banner-img img{
+        width: 500px;
+        animation: animated-img 4s linear infinite;
+    } 
+}
+
+.d-middle{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+@keyframes animated-img {
+    30%{
+        transform: translateY(-15px);
+    }
+    60%{
+        transform: translateY(30px);
+    }
+}
+</style>
